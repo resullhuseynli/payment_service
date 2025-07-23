125ba685bbd3a99aed274ff41a621a0fda12c86d
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-23 22:31:17 +0400

    userEntity added

---
+++ src/main/java/com/service/payment/dao/model/UserEntity.java
@@ -0,0 +1,40 @@
+package com.service.payment.dao.model;
+
+import com.service.payment.enums.UserStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_first_name")
+    private String firstName;
+
+    @Column(name = "user_last_name")
+    private String lastName;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Digits(integer = 10, fraction = 2)
+    @Column(name = "user_balance")
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_status")
+    private UserStatus status;
+}
---
+++ src/main/java/com/service/payment/enums/UserStatus.java
@@ -0,0 +1,7 @@
+package com.service.payment.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    DELETED,
+    INACTIVE
+}
