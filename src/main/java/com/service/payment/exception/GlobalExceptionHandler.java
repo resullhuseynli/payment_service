50de761531beb1a1395efc5e55bfbfffb35f16cd
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-25 12:31:19 +0400

    kafka added

--- src/main/java/com/service/payment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/service/payment/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.service.payment.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,17 +11,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InsufficientMoneyException.class)
     public ResponseEntity<ErrorResponse<String>> handleInsufficientMoneyException(InsufficientMoneyException insufficientMoneyException) {
-        return ResponseEntity.badRequest().body(new ErrorResponse<>(UUID.randomUUID(),insufficientMoneyException.getMessage()));
+        return ResponseEntity.badRequest().body(new ErrorResponse<>(UUID.randomUUID(), insufficientMoneyException.getMessage()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse<String>> handleNotFoundException(NotFoundException notFoundException) {
-        return ResponseEntity.badRequest().body(new ErrorResponse<>(UUID.randomUUID(),notFoundException.getMessage()));
+        return ResponseEntity.status(NOT_FOUND).body(new ErrorResponse<>(UUID.randomUUID(), notFoundException.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -32,7 +34,7 @@ public class GlobalExceptionHandler {
                 .stream()
                 .map(FieldError::getField)
                 .toList();
-        return new ResponseEntity<>(new ErrorResponse<>(UUID.randomUUID(), getErrorsMap(errors)), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse<>(UUID.randomUUID(), getErrorsMap(errors)), BAD_REQUEST);
     }
 
     private Map<String, List<String>> getErrorsMap(List<String> errors) {
---
+++ src/main/java/com/service/payment/service/PaymentService.java
@@ -0,0 +1,45 @@
+package com.service.payment.service;
+
+import com.service.payment.dao.model.UserEntity;
+import com.service.payment.exception.InsufficientMoneyException;
+import com.service.payment.exception.NotFoundException;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class PaymentService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final UserService userService;
+
+    public PaymentService(KafkaTemplate<String, String> kafkaTemplate, UserService userService) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.userService = userService;
+    }
+
+    @KafkaListener(topics = "payment-request", groupId = "payment-service")
+    public void handleRequest(String message) {
+        String[] parts = message.split(":");
+        Long userId = Long.parseLong(parts[0]);
+        String requestId = parts[1];
+        BigDecimal amount = BigDecimal.valueOf(Double.parseDouble(parts[2]));
+        StringBuilder response = new StringBuilder(userId.toString());
+        response.append(":").append(requestId);
+        try {
+            userService.buyBook(userId, amount);
+        } catch (InsufficientMoneyException insufficientMoneyException) {
+            response.append(":FAILURE");
+            kafkaTemplate.send("payment-response", String.valueOf(response));
+            System.err.println("Insufficient money exception occurred");
+        } catch (NotFoundException notFoundException) {
+            response.append(":FAILURE");
+            kafkaTemplate.send("payment-response", String.valueOf(response));
+            System.err.println("Not Found exception occurred");
+        }
+        response.append(":SUCCESS");
+        kafkaTemplate.send("payment-response", String.valueOf(response));
+    }
+}
